911e93b9f8ddb97bf255f86e08e5dc37779201dc
walnka <willemdesu@gmail.com>
2021-11-28 21:35:32 -0800

    Setup index.html and started implementing login page. Php is a bit tough at the moment...

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+
+if (isset($_POST['login']) && !empty($_POST['username']) 
+               && !empty($_POST['password'])) {
+				
+               if ($_POST['username'] == 'tutorialspoint' && 
+                  $_POST['password'] == '1234') {
+                  $_SESSION['valid'] = true;
+                  $_SESSION['timeout'] = time();
+                  $_SESSION['username'] = 'tutorialspoint';
+                  
+                  echo 'You have entered valid use name and password';
+               }
+               else {
+                  $msg = 'Wrong username or password';
+               }
+            }
+
+
+?>
