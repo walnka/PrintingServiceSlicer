7613f342085a1e71ecc4b12a1c5d67ed011c75c0
walnka <willemdesu@gmail.com>
2022-03-01 20:09:44 -0800

    Fixed upload directory

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 
 <?php
-$target_dir = "uploads/";
+$target_dir = "/var/www/printquote/PrintingServiceSlicer/uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
